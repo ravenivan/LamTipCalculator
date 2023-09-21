0bc9bde6844d9ed9d066f2302fc2292aacdcdb5a
Ivan <ravenivan1@gmail.com>
2023-09-21 16:38:47 -0400

    Finished!

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,3 +1,8 @@
+/*
+This program is a Tip Calculator
+It can be used to help users figure out how much tip they have to pay.
+ */
+
 import java.util.Scanner;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
@@ -6,25 +11,25 @@ public class TipCalculator {
 
     public static void main(String[] args) {
 
-
-        String[] itemList = new String[100];
+        String[] itemList = new String[100]; // array of user inputted items
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to the tip calculator! ");
         System.out.print("What is your name? ");
-        String name = sc.nextLine();
+        String name = sc.nextLine(); // stores name of user
 
         System.out.println("Hello " + name + "!" + " How many people are in your group today?: ");
-        int people = sc.nextInt();
+        int people = sc.nextInt(); // stores number of people in the group
 
         System.out.println("What tip percentage would you like to give? (0-100): ");
-        int tip = sc.nextInt();
+        int tip = sc.nextInt(); // stores tip
 
         String item;
         double costItem = 0;
         double totalCost = 0;
 
+        // User inputs cost and item
         for (int i = 0; costItem != -1; i++) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             costItem = sc.nextDouble();
@@ -39,6 +44,40 @@ public class TipCalculator {
             }
         }
 
+        double costAfterTip = totalCost * ((tip/100.0) + 1);
+        double totalTip = costAfterTip - totalCost;
+        double perCost1 = totalCost / people;
+        double tipPer = totalTip / people;
+        double perCost2 = costAfterTip / people;
+
+        // Full bill information
+        System.out.println("----------------------------------");
+        System.out.println("Total bill before tip: $" + df.format(totalCost));
+        System.out.println("Total percentage: " + tip + "%");
+        System.out.println("Total tip : $" + df.format(totalTip));
+        System.out.println("Total bill with tip: $" + df.format(costAfterTip));
+        System.out.println("Per person cost before tip: $" + df.format(perCost1));
+        System.out.println("Tip per person: $" + df.format(tipPer));
+        System.out.println("Total cost per person: $" + df.format(perCost2));
+        System.out.println("----------------------------------");
+
+        // Prints out all items that the user inputted
+        System.out.println("Items ordered: ");
+        for (String j: itemList) {
+            if (j != null) {
+                System.out.println(j);
+            }
+        }
+
+        // Checks to see if tip is >= 15 percent. (Freestyle)
+        System.out.println("----------------------------------");
+        if (tip >= 15) {
+            System.out.println("What a generous tip!");
+            System.out.println("Have a good day " + name + "!");
+        } else {
+            System.out.println("What a low tip!");
+            System.out.println("Never come back again " + name + "!");
+        }
     }
 }
 
