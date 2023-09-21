4550516ff2d1057bacadf8ba0fa63cc4e73b95c3
Ivan <ravenivan1@gmail.com>
2023-09-21 16:20:27 -0400

    Added a string array and a scanner object

---
+++ src/TipCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+public class TipCalculator {
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    public static void main(String[] args) {
+
+
+        String[] itemList = new String[100];
+
+        Scanner sc = new Scanner(System.in);
+
+
+
